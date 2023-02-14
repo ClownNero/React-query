b319e09c56263467b244169318d7aee34dd03318
ClownNero <ClownNero@gmail.com>
2023-02-14 19:30:30 +0900

    second

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,12 +1,19 @@
 import React, { useState } from 'react';
-import useProducts from '../hooks/use-products';
-
+import {useQuery} from '@tanstack/react-query';
 export default function Products() {
   const [checked, setChecked] = useState(false);
-  const [loading, error, products] = useProducts({ salesOnly: checked });
+  const {isLoading, error, data: products} = useQuery(['products', checked], async()=>{
+    console.log('fetching...', checked);
+    return fetch(`data/${checked ? 'sale_' : ''}products.json`)
+    .then((res) => res.json());
+  },{
+    staleTime: 1000*60*5, // 5분동안 캐시된 데이터 사용하기
+
+  });
+  // const [loading, error, products] = useProducts({ salesOnly: checked });
   const handleChange = () => setChecked((prev) => !prev);
 
-  if (loading) return <p>Loading...</p>;
+  if (isLoading) return <p>Loading...</p>;
 
   if (error) return <p>{error}</p>;
 
